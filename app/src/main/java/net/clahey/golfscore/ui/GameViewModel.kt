7f526e830936d7240167029eb16b9e8d87338596
Chris Lahey <clahey@clahey.net>
2024-10-15 21:22:18 -0400

    Code cleanup

--- app/src/main/java/net/clahey/golfscore/ui/GameViewModel.kt
+++ app/src/main/java/net/clahey/golfscore/ui/GameViewModel.kt
@@ -19,6 +19,25 @@ import net.clahey.golfscore.data.database.Score
 import net.clahey.immutable.copy
 import java.lang.Integer.max
 
+data class Hole(val scores: Map<Int, Int>, val label: String) {
+
+    fun getPlayerScore(player: Int): Int? {
+        return scores[player]
+    }
+
+    fun setPlayerScore(player: Int, score: Int): Hole = copy(scores = scores.copy(player to score))
+
+    fun clearPlayerScore(player: Int): Hole = copy(scores = buildMap {
+        putAll(scores)
+        remove(player)
+    })
+}
+
+data class Player(val name: String, val id: Int)
+data class GameConfig(val players: List<Player>, val title: String, val holeCount: Int) {
+    constructor() : this(listOf(), "", 0)
+}
+
 data class GameState (val holes: List<Hole>, val playerScores: Map<Int, Int>)
 
 class GameViewModel(
@@ -30,11 +49,7 @@ class GameViewModel(
     private val _uiState = MutableStateFlow(GameState(listOf(), mapOf()))
     val uiState: StateFlow<GameState> = _uiState.asStateFlow()
     val gameConfig: Flow<GameConfig> = db.gameDao().getWithPlayersAndScores(gameId).map {
-        if (it != null) {
-            GameConfig(it.players.map { Player(it.name, it.id) }, it.game.title, it.game.holeCount)
-        } else {
-            GameConfig()
-        }
+        GameConfig(it.players.map { Player(it.name, it.id) }, it.game.title, it.game.holeCount)
     }
 
     private fun launchDb(block: () -> Unit) {
@@ -67,8 +82,8 @@ class GameViewModel(
             ) { mutableMapOf<Int, Int>() }
             val totals = mutableMapOf<Int, Int>()
             for (score in scores) {
-                holeMaps[score.hole].put(score.player, score.score)
-                totals[score.player] = totals[score.player] ?: 0 + score.score
+                holeMaps[score.hole][score.player] = score.score
+                totals[score.player] = (totals[score.player] ?: 0) + score.score
             }
             _uiState.update {
                 GameState(holeMaps.mapIndexed { index, map -> Hole(map, (index + 1).toString()) }, totals)
@@ -76,11 +91,11 @@ class GameViewModel(
         }
     }
 
-    fun setScore(player: Int, hole: Int, score: Int) {
+    private fun setScore(player: Int, hole: Int, score: Int) {
         launchDb {
 
             // check parameters
-            val total = uiState.value.holes.map {it.scores[player] ?: 0}.sum() - (uiState.value.holes[hole].scores[player] ?: 0) + score
+            val total = uiState.value.holes.sumOf { it.scores[player] ?: 0 } - (uiState.value.holes[hole].scores[player] ?: 0) + score
             db.gameDao().setScore(gameId, player, hole, score, total)
             _uiState.update {
                 it.copy(holes = it.holes.copy(
@@ -91,7 +106,7 @@ class GameViewModel(
     }
 
     private fun clearScore(playerId: Int, hole: Int) {
-        val total = uiState.value.holes.map {it.scores[playerId] ?: 0}.sum() - (uiState.value.holes[hole].scores[playerId] ?: 0)
+        val total = uiState.value.holes.sumOf { it.scores[playerId] ?: 0 } - (uiState.value.holes[hole].scores[playerId] ?: 0)
         launchDb {
             // check parameters
             db.gameDao().clearScore(gameId, playerId, hole, total)
@@ -103,23 +118,4 @@ class GameViewModel(
         }
     }
 
-}
-
-data class Hole(val scores: Map<Int, Int>, val label: String) {
-
-    fun getPlayerScore(player: Int): Int? {
-        return scores[player]
-    }
-
-    fun setPlayerScore(player: Int, score: Int): Hole = copy(scores = scores.copy(player to score))
-
-    fun clearPlayerScore(player: Int): Hole = copy(scores = buildMap {
-        putAll(scores)
-        remove(player)
-    })
-}
-
-data class Player(val name: String, val id: Int)
-data class GameConfig(val players: List<Player>, val title: String, val holeCount: Int) {
-    constructor() : this(listOf(), "", 0)
 }
