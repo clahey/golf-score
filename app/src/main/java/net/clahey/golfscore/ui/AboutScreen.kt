a05c1414147b3ff09ee136e8dbb03129f75d7710
Chris Lahey <clahey@clahey.net>
2024-10-15 21:29:21 -0400

    Add unused About Screen including thanks.

---
+++ app/src/main/java/net/clahey/golfscore/ui/AboutScreen.kt
@@ -0,0 +1,9 @@
+package net.clahey.golfscore.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AboutScreen() {
+    Text("Icon based on https://creazilla.com/media/clipart/14597/golf-hole")
+}
