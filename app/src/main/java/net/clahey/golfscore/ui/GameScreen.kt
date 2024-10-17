68f4679d768695377bc188bc1f41492a7e1c9d46
Chris Lahey <clahey@clahey.net>
2024-10-16 22:05:45 -0400

    Improved game screen UI.

--- app/src/main/java/net/clahey/golfscore/ui/GameScreen.kt
+++ app/src/main/java/net/clahey/golfscore/ui/GameScreen.kt
@@ -2,6 +2,7 @@ package net.clahey.golfscore.ui
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -21,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -71,8 +74,7 @@ fun GameScreen(
             })
     }) { innerPadding ->
         Column(Modifier.padding(innerPadding)) {
-            GameScoreDisplay(
-                gameConfig,
+            GameScoreDisplay(gameConfig,
                 gameUiState.holes,
                 onShowScoreUpdateDialog = { player: Int, hole: Int ->
                     onChangeScore(
@@ -93,23 +95,29 @@ fun GameScoreDisplay(
     onShowScoreUpdateDialog: ((Int, Int) -> Unit)? = null,
 ) {
     LazyVerticalGrid(columns = GridCells.Fixed(gameConfig.players.size + 1), Modifier.outline()) {
-        item(key = 0) { Text("Hole", Modifier.outline()) }
+        item(key = 0) { TextBox("Hole", textAlign = CenterEnd) }
         for (player in gameConfig.players) {
-            item(key = Pair(0, player.id)) { Text(player.name, Modifier.outline()) }
+            item(key = Pair(0, player.id)) { TextBox(player.name,) }
         }
         val runningTotals = mutableMapOf<Int, Int>()
         for (i in 0..<gameConfig.holeCount) {
             val hole = holes.getOrElse(i) { Hole(mapOf(), (i + 1).toString()) }
-            item(key = i + 1) { Text(hole.label, Modifier.outline()) }
+            item(key = i + 1) {
+                TextBox(
+                    hole.label,
+                    textAlign = CenterEnd,
+                )
+            }
             for ((player, score) in hole.scores) {
                 runningTotals[player] = (runningTotals[player] ?: 0) + score
             }
             for (player in gameConfig.players) {
+                val runningTotal = runningTotals[player.id]
                 item(key = Pair(i + 1, player.id)) {
                     ScoreDisplay(hole.scores[player.id],
-                        onShowScoreUpdateDialog = onShowScoreUpdateDialog?.let { f ->
-                            { f(player.id, i) }
-                        })
+                        runningTotal ?: 0,
+                        onClick = onShowScoreUpdateDialog?.let { f -> { f(player.id, i) } }
+                    )
                 }
             }
         }
@@ -117,15 +125,32 @@ fun GameScoreDisplay(
 }
 
 @Composable
-fun ScoreDisplay(score: Int?, onShowScoreUpdateDialog: (() -> Unit)? = null) {
-    val modifier = if (onShowScoreUpdateDialog != null) {
-        Modifier.clickable(onClick = onShowScoreUpdateDialog)
-    } else {
-        Modifier
-    }
-    Text(
-        score?.toString() ?: "", modifier.outline()
-    )
+fun ScoreDisplay(score: Int?, runningTotal: Int, onClick: (() -> Unit)?) {
+    TextBox(if (score == null) "" else "$score ($runningTotal)", onClick = onClick)
 }
 
 fun Modifier.outline(): Modifier = border(0.5.dp, SolidColor(Color.Black), RectangleShape)
+
+@Composable
+fun TextBox(
+    text: String,
+    boxModifier: Modifier = Modifier,
+    textAlign: Alignment? = null,
+    onClick: (() -> Unit)? = null
+) {
+    val boxModifier = if (onClick != null) {
+        boxModifier.clickable(onClick = onClick)
+    } else {
+        boxModifier
+    }
+    Box(modifier = boxModifier.outline()) {
+        val textModifier = if (textAlign != null) {
+            Modifier.align(textAlign)
+        } else {
+            Modifier
+        }
+        Text(
+            text, textModifier.padding(8.dp, 2.dp)
+        )
+    }
+}
