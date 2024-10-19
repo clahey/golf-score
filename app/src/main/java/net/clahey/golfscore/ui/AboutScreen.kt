b5ef146bdbd263175e85636c0ffb7c26eae5961e
Chris Lahey <clahey@clahey.net>
2024-10-18 22:59:10 -0400

    Add a link to the source code to the About page.

--- app/src/main/java/net/clahey/golfscore/ui/AboutScreen.kt
+++ app/src/main/java/net/clahey/golfscore/ui/AboutScreen.kt
@@ -1,6 +1,7 @@
 package net.clahey.golfscore.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -9,37 +10,54 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withLink
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AboutScreen() {
+    val uriHandler = LocalUriHandler.current
+
+    @Composable
+    fun AnnotatedString.Builder.withLink(
+        link: String,
+        block: AnnotatedString.Builder.() -> Unit,
+    ) {
+        val link = LinkAnnotation.Url(link, TextLinkStyles(SpanStyle(color = Color.Blue))) {
+            val url = (it as LinkAnnotation.Url).url
+            // log some metrics
+            uriHandler.openUri(url)
+        }
+        withLink(link) {
+            block()
+        }
+    }
+
     Column {
-        Text("Minigolf ScoreCard", style = MaterialTheme.typography.displayMedium, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Text(
+            "GolfScore",
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        )
         HorizontalDivider()
-        Text("By Your Average Chris")
+        Text("By Your Average Chris", Modifier.padding(8.dp))
+        Text(buildAnnotatedString {
+            append("Source code available at ")
+            withLink("https://github.com/clahey/golf-score") { append("github.com/clahey/golf-score") }
+        }, Modifier.padding(8.dp))
         HorizontalDivider()
 
-        // Display a link in the text and log metrics whenever user clicks on it. In that case we handle
-        // the link using openUri method of the LocalUriHandler
-        val uriHandler = LocalUriHandler.current
         Text(
             buildAnnotatedString {
                 append("Icon based on  ")
-                val link =
-                    LinkAnnotation.Url(
-                        "https://creazilla.com/media/clipart/14597/golf-hole",
-                        TextLinkStyles(SpanStyle(color = Color.Blue))
-                    ) {
-                        val url = (it as LinkAnnotation.Url).url
-                        // log some metrics
-                        uriHandler.openUri(url)
-                    }
-                withLink(link) { append("https://creazilla.com/media/clipart/14597/golf-hole") }
-            }
+                withLink("https://creazilla.com/media/clipart/14597/golf-hole") { append("https://creazilla.com/media/clipart/14597/golf-hole") }
+            }, Modifier.padding(8.dp)
         )
     }
 }
